8b87519407317f6e5fb40f225de88aef450c0382
Junaed <junaed1995@gmail.com>
2023-05-22 23:07:41 +0600

    Initial Commit

---
+++ NoteAppFirebase_iOS/NoteAppFirebase_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppFirebase_iOSApp.swift
+//  NoteAppFirebase_iOS
+//
+//  Created by Junaed Muhammad Chowdhury on 22/5/23.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppFirebase_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
